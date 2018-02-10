5e753555c79bce0d3d08fb0e1472e5f97ca54682
Luis Garzon <luisgarzon90@yahoo.com>
2018-02-10 13:41:21 -0500

    Created the General Layout of the simon Game boxes

--- src/Simon.js
+++ src/Simon.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 class Simon extends Component{
     render(){
         return (
-            <h1>Hello World!</h1>
+        <div>
+        <div className= "d-flex justify-content-center col-warning">
+           <div className="row hello">           
+                <div className="col bg-success form-control-lg">col</div>
+                <div className="col bg-danger form-control-lg">col</div>
+                <div className="w-100"></div>
+                <div className="col bg-warning form-control-lg ">col</div>
+                <div className="col bg-primary form-control-lg">col</div>
+            </div>
+        </div>   
+        </div> 
         );
     }
 }
